475813af58ca2ab7e9c0b0b9460e02e41475cc72
varakumar <vignesh.varakumar@gmail.com>
2016-03-30 16:14:26 +0530

    First commit

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='add_numbers',
+setup(name='varakumar1105_add_numbers',
       version='0.1',
       description='Sample to add two numbers',
-      url='https://github.com/varakumar1105/add_numbers',
+      url='https://github.com/varakumar1105/varakumar1105_add_numbers',
       author='VaraKumarJagarapu',
       author_email='vignesh.varakumar@gmail.com',
       license='MIT',
-      packages=['add_numbers'],
+      packages=['varakumar1105_add_numbers'],
       zip_safe=False)
 #My First Commit with joke function
