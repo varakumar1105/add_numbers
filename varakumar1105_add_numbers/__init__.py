7fcf179a19586fc5fa7fb4e33f2813adbca6034d
varakumar <vignesh.varakumar@gmail.com>
2016-03-30 16:22:44 +0530

    First commit

--- varakumar1105_add_numbers/__init__.py
+++ varakumar1105_add_numbers/__init__.py
@@ -1,3 +1,6 @@
 def joke():
     return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
             u'Beiherhund das Oder die Flipperwaldt gersput.')
+
+def add(a,b):
+    return "The sum ==> "+str(a+b)
