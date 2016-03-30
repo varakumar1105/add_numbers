e7f17f6018720c966779bbf35e169e2f97b94451
varakumar <vignesh.varakumar@gmail.com>
2016-03-30 15:55:20 +0530

    My First Commit with joke function

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='add_numbers',
+      version='0.1',
+      description='Sample to add two numbers',
+      url='https://github.com/varakumar1105/add_numbers',
+      author='VaraKumarJagarapu',
+      author_email='vignesh.varakumar@gmail.com',
+      license='MIT',
+      packages=['add_numbers'],
+      zip_safe=False)
+#My First Commit with joke function
